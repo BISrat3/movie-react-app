67d2a3c6b1808d0626db53ac7989749779c26c06
bisrat <bisrat.amtataw@gmail.com>
2022-11-08 09:17:01 -0500

    error handling

--- movie-react-app/src/App.js
+++ movie-react-app/src/App.js
@@ -5,12 +5,18 @@ import MovieList from "./components/MovieList";
 function App() {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   async function fetchMovieHandler(){
     setIsLoading(true)
+    setError(null)
+    try {
     const response = await fetch('https://swapi.dev/api/films/')
+    if (!response.ok){
+      throw new Error('Something went wrong')
+    }
     const data = await response.json()
-    
+
         const transformedMovies = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
@@ -21,8 +27,25 @@ function App() {
           }
         })
         setMovies(transformedMovies)
-        setIsLoading(false)
       }
+      catch (error) { 
+        setError(error.message)
+      }
+      setIsLoading(false)
+    }
+
+    let content = <p>Found no movies</p>
+
+    if(movies.length >0){
+      content = <MovieList movies={movies} />
+    }
+    if(movies.length === 0 ){
+      content = <p>{error}</p>
+    }
+    if(isLoading){
+      content =  <p>Loading.....</p>
+    }
+    
 
   return (
     <React.Fragment >
@@ -30,8 +53,7 @@ function App() {
         <button onClick={fetchMovieHandler}> Fetch Movies</button>
       </section>
       <section>
-      { !isLoading && <MovieList movies={movies} />}
-      {isLoading && <p>Loading.....</p>}
+        {content}
       </section>
     </React.Fragment>
   );
